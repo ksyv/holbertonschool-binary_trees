b2f3e71b67fc0d53f4f6c4bb87f5b23ff0125d5b
ksyv <viais.kevin@hotmail.fr>
2024-01-08 11:15:01 +0100

    add condition if parent is NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *newNode;
 
 		newNode = malloc(sizeof(binary_tree_t));
-		if (newNode == NULL)
+		if (newNode == NULL || parent == NULL)
 			return (NULL);
 		if (parent->right != NULL)
 		{
