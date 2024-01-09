fb97ca3e397126b8f11588b7478a787950b2d8ae
ksyv <viais.kevin@hotmail.fr>
2024-01-09 09:55:35 +0100

    fix 16-.c add condition of each node is perfect balanced

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,11 +7,17 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int balance = binary_tree_balance(tree);
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
 	if (balance == 0)
-		return (1);
+	{
+		left = binary_tree_balance(tree->left);
+		right = binary_tree_balance(tree->right);
+		if (left == 0 && right == 0)
+			return (1);
+	}
 	return (0);
 }
 /**
